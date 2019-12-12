39a0312951a7b40e55911e93470234a9bdedf4f5
Malachi Schram <malachi.schram@gmail.com>
2019-12-12 13:56:51 -0800

    register new environments

--- exa_gym/__init__.py
+++ exa_gym/__init__.py
@@ -4,3 +4,14 @@ register(
     id='ch-v0',
     entry_point='envs:CahnHilliardEnv',
 )    
+
+register(
+    id='ExaLearnBlockCoPolymerTDLG-v0',
+    entry_point='exa_gym.envs:BlockCoPolymerTDLG',
+    kwargs={'cfg_file': 'cfg/tdlg_setup.json'}
+)
+
+register(
+    id='ExaLearnCartpole-v0',
+    entry_point='exa_gym.envs:ExaCartpole',
+)
