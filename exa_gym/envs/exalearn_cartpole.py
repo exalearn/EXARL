a75a379b8d77961d83f77c5f1f2be45b164d674c
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2019-12-18 15:43:44 -0700

    Added config file for mpi env
    env_cfg

--- exa_gym/envs/exalearn_cartpole.py
+++ exa_gym/envs/exalearn_cartpole.py
@@ -5,15 +5,17 @@ import time
 from mpi4py import MPI
 import numpy as np
 import sys
+import json
 
 class ExaCartpole(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
+    def __init__(self, cfg='exa_gym/envs/env_cfg/env_setup.json'):
         self.env = gym.make('CartPole-v0')
         #self.env = gym.make('FrozenLake-v0')
         self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
+        self.cfg = cfg
 
     def step(self, action):
         next_state, reward, done, info = self.env.step(action)
@@ -23,9 +25,12 @@ class ExaCartpole(gym.Env):
         
         # Compute PI with dynamic process spawning
         worker = './exa_gym/envs/cpi.py'
+        with open(self.cfg) as json_file:
+            data = json.load(json_file)
+        num_child_per_parent = int(data['child_spawn_per_parent']) if 'child_spawn_per_parent' in data.keys() else 1
         comm = MPI.COMM_SELF.Spawn(sys.executable,
                                    args=[worker],
-                                   maxprocs=2)
+                                   maxprocs=num_child_per_parent)
 
         N = np.array(100, 'i')
         comm.Bcast([N, MPI.INT], root=MPI.ROOT)
