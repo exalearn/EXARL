32ceb368473fb0c066b61f377e1ed4378c837119
Malachi Schram <malachi.schram@gmail.com>
2019-12-20 14:47:36 -0800

    add 3d bcp

--- exa_gym_envs/envs/__init__.py
+++ exa_gym_envs/envs/__init__.py
@@ -1,2 +1,2 @@
-#from exa_gym.envs.exalearn_bcp_tdlg import BlockCoPolymerTDLG
+from exa_gym_envs.envs.exalearn_bcp_tdlg import BlockCoPolymerTDLG
 from exa_gym_envs.envs.exalearn_cartpole import ExaCartpole
