242744a84f1a44ac4f526fef1e31e1ab00c3cfe1
aik7 <ai.kagwa@gmail.com>
2020-01-09 10:23:09 -0500

    fixed gym environment for 2D CH

--- exa_envs/__init__.py
+++ exa_envs/__init__.py
@@ -1,9 +1,9 @@
 from gym.envs.registration import register
 
-#register(
-#    id='ch-v0',
-#    entry_point='envs:CahnHilliardEnv',
-#)    
+register(
+    id='ch-v0',
+    entry_point='exa_envs.envs:CahnHilliardEnv',
+)    
 
 register(
     id='ExaLearnBlockCoPolymerTDLG-v0',
