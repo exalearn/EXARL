577e37a9cc6fe53cbdc1b200ccb32f268711d820
Malachi Schram <malachi.schram@gmail.com>
2020-01-13 09:19:41 -0800

    Update examples results directories

--- mpi_scripts/exa_learner_example2.py
+++ mpi_scripts/exa_learner_example2.py
@@ -6,6 +6,6 @@ env_id   = 'exa_envs:ExaLearnCartPole-v0'
 
 ## Create ExaDQN
 exa_learner = ExaLearner(agent_id,env_id)
-exa_learner.set_results_dir('./exa_learner_results/')
+exa_learner.set_results_dir('./exa_learner_example2_results/')
 exa_learner.set_training(10,10)
 exa_learner.run()
