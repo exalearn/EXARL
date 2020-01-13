54c4b13bd4d0f6de6fd411854bc492482f3884be
Malachi Schram <malachi.schram@gmail.com>
2020-01-13 09:48:13 -0800

    Update to use exa_rl

--- mpi_scripts/exa_learner_example1.py
+++ mpi_scripts/exa_learner_example1.py
@@ -1,4 +1,4 @@
-from exa_base.exa_learner import ExaLearner
+from exa_rl.exa_learner import ExaLearner
 
 ## Define agent and env
 agent_id = 'exa_agents:DQN-v0'
@@ -7,5 +7,5 @@ env_id   = 'CartPole-v0'
 ## Create ExaLearner
 exa_learner = ExaLearner(agent_id,env_id)
 exa_learner.set_results_dir('./exa_learner_example1_results/')
-exa_learner.set_training(10,10)
+exa_learner.set_training(100,100)
 exa_learner.run()
--- mpi_scripts/exa_learner_example2.py
+++ mpi_scripts/exa_learner_example2.py
@@ -1,4 +1,4 @@
-rom exa_base.exa_learner import ExaLearner
+from exa_rl.exa_learner import ExaLearner
 
 ## Define agent and env
 agent_id = 'exa_agents:DQN-v0'
@@ -7,5 +7,5 @@ env_id   = 'exa_envs:ExaLearnCartPole-v0'
 ## Create ExaDQN
 exa_learner = ExaLearner(agent_id,env_id)
 exa_learner.set_results_dir('./exa_learner_example2_results/')
-exa_learner.set_training(10,10)
+exa_learner.set_training(100,100)
 exa_learner.run()
