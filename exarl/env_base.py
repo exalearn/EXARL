15a4b03b52259c9f8cbadbb7848f1731fa2465f0
schr476 <48308123+schr476@users.noreply.github.com>
2020-01-22 09:38:55 -0800

    Update env_base.py
    Add TODO

--- exarl/env_base.py
+++ exarl/env_base.py
@@ -11,6 +11,7 @@ class env_base():
         with open(self.env_cfg) as json_file:
             env_data = json.load(json_file)
 
+        ## TODO: We need to define defaults for MPI, GPU, OMP, etc. and exit code if invalid parameters ##
         self.num_child_per_parent = int(env_data['child_spawn_per_parent']) if 'child_spawn_per_parent' in env_data.keys() else 0
         if(self.num_child_per_parent > 0):
             # defaults to running toy example of computing PI
