d9ff7c849f670a125366668e7bc2360bc0e0fed2
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2020-01-23 08:22:45 -0700

    changing mpi script name

--- mpi_scripts/exalearn_example.py
+++ mpi_scripts/exalearn_example.py
@@ -6,10 +6,10 @@ agent_id = 'agents:DQN-v0'
 env_id   = 'envs:ExaLearnCartpole-v0'
 
 ## Create ExaDQN
-exa_dqn = erl.ExaLearner(agent_id,env_id)
-exa_dqn.set_results_dir('./exa_dqn_results/')
-exa_dqn.set_training(10,10)
+exa_learner = erl.ExaLearner(agent_id,env_id)
+exa_learner.set_results_dir('./exa_dqn_results/')
+exa_learner.set_training(10,10)
 start = time.time()
-exa_dqn.run()
+exa_learner.run()
 stop = time.time()
 print("Elapsed time = ", stop - start)
