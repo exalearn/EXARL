1d259b27f013cd5268c3392fad8d30da60863f70
Ai Kagawa <aik07@batch2.summit.olcf.ornl.gov>
2020-04-22 14:39:50 -0400

    need to fix indentation

--- envs/env_vault/__init__.py
+++ envs/env_vault/__init__.py
@@ -1,3 +1,4 @@
 from envs.env_vault.exalearn_bcp_tdlg import BlockCoPolymerTDLG
 from envs.env_vault.exalearn_cartpole import ExaCartpole
 from envs.env_vault.exalearn_cartpole_static import ExaCartpoleStatic
+from envs.env_vault.env_ch                   import CahnHilliardEnv 
--- envs/env_vault/exalearn_cartpole_static.py
+++ envs/env_vault/exalearn_cartpole_static.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 import json
 import exarl as erl
-from envs.env_vault.computePI import computePI as cp
+#from envs.env_vault.computePI import computePI as cp
 
 def computePI(N,new_comm):
     h = 1.0 / N
@@ -47,8 +47,8 @@ class ExaCartpoleStatic(gym.Env, erl.ExaEnv):
         N = comm.bcast(N, root=0)	
         color = int(world_rank/(self.mpi_children_per_parent+1))
         newcomm = comm.Split(color, world_rank)
-        #myPI = computePI(N, newcomm)
-        myPI = cp.compute_pi(N, newcomm)
+        myPI = computePI(N, newcomm)
+        #myPI = cp.compute_pi(N, newcomm)
         PI = newcomm.reduce(myPI, op=MPI.SUM, root=0)
         newrank = newcomm.rank
         #if newrank == 0:
