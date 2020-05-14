3cf6e0188c6983b3f250bcf53fa7435b11eff10c
Jamal <jamal@lanl.gov>
2020-05-14 08:31:44 -0600

    Add base_parser to overwrite agent and env before reading json files.

--- exarl/learner_base.py
+++ exarl/learner_base.py
@@ -103,7 +103,7 @@ class ExaLearner():
 
     def run_exarl(self, comm):
 
-        filename_prefix = 'ExaLearner_' + 'Episode%s_Steps%s_Rank%s_memory_v1' % ( str(self.nepisodes), str(self.nsteps), str(comm.rank))
+        filename_prefix = 'ExaLearner_' + 'Episodes%s_Steps%s_Rank%s_memory_v1' % ( str(self.nepisodes), str(self.nsteps), str(comm.rank))
         train_file = open(self.results_dir+'/'+filename_prefix + ".log", 'w')
         train_writer = csv.writer(train_file, delimiter = " ")
         #print('self.world_comm.rank:',self.world_comm.rank)
