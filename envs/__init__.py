6c63ddcd4214bfa861096ee4d3fc16fa606f00fe
Jamal <jamal@lanl.gov>
2020-05-15 09:11:28 -0600

    Removed hard-coded default json file from registration

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,12 +3,11 @@ from gym.envs.registration import register
 register(
     id='ch-v0',
     entry_point='envs.env_vault:CahnHilliardEnv',
-)    
+)
 
 register(
     id='ExaLearnBlockCoPolymerTDLG-v0',
     entry_point='envs.env_vault:BlockCoPolymerTDLG',
-    kwargs={'cfg_file': 'envs/env_vault/env_cfg/tdlg_setup.json'}
 )
 
 register(
