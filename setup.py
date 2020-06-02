2f1aa16800439c932e27c0fa4fbbd1c41c19f5ac
aik7 <ai.kagwa@gmail.com>
2020-06-02 14:01:15 -0400

    test travis-ci

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='exarl',
       version='0.0.1',
-      install_requires=['ase','Lmfit','keras','mpi4py'] # dependencies #gym
+      install_requires=['ase','Lmfit','keras'] # dependencies #gym, mpi4py
 )
