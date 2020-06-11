051fb5d25cb5a99c01a542b1cb0a8cfbdd1b5305
rvinaybharadwaj <vinayr@lanl.gov>
2020-06-11 17:21:06 -0400

    adding conditional statements to registration

--- envs/env_vault/__init__.py
+++ envs/env_vault/__init__.py
@@ -1,6 +1,16 @@
-from envs.env_vault.exalearn_bcp_tdlg_v3 import BlockCoPolymerTDLG
-from envs.env_vault.ExaCartpoleDynamic import ExaCartpoleDynamic
-from envs.env_vault.ExaCartpoleStatic import ExaCartpoleStatic
-#from envs.env_vault.env_ch import CahnHilliardEnv
-#from envs.env_vault.ExaCOVID import ExaCOVID
+import driver.candleDriver as cd
+
+run_params = cd.initialize_parameters()
+env = run_params['env']
+
+if env == 'ExaLearnBlockCoPolymerTDLG-v0':
+    from envs.env_vault.exalearn_bcp_tdlg import BlockCoPolymerTDLG
+elif env == 'ExaLearnCartpole-v0':
+    from envs.env_vault.ExaCartpoleDynamic import ExaCartpoleDynamic
+elif env == 'ExaLearnCartpole-v1':
+    from envs.env_vault.ExaCartpoleStatic import ExaCartpoleStatic
+elif env == 'ch-v0':
+    from envs.env_vault.env_ch import CahnHilliardEnv
+elif env == 'ExaCovid-v0':
+    from envs.env_vault.ExaCOVID import ExaCOVID
 
