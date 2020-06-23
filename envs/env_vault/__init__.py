fb54fb13b7a2d2c12e2d9f2b153b491c7fcee2e8
schr476 <malachi.schram@pnnl.gov>
2020-06-23 14:49:29 -0700

    Update the __init__.py

--- envs/env_vault/__init__.py
+++ envs/env_vault/__init__.py
@@ -5,6 +5,8 @@ env = run_params['env']
 
 if env == 'ExaLearnBlockCoPolymerTDLG-v0':
     from envs.env_vault.exalearn_bcp_tdlg import BlockCoPolymerTDLG
+elif env == 'ExaLearnBlockCoPolymerTDLG-v3':
+    from envs.env_vault.exalearn_bcp_tdlg_v3 import BlockCoPolymerTDLG
 elif env == 'ExaLearnCartpole-v0':
     from envs.env_vault.ExaCartpoleDynamic import ExaCartpoleDynamic
 elif env == 'ExaLearnCartpole-v1':
