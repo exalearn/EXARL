985fa6d9fc81e2344ed5e36099d9f6fc06afc5ac
schr476 <malachi.schram@pnnl.gov>
2020-07-17 16:01:56 -0700

    Update the env_base to use the wrapper

--- exarl/env_base.py
+++ exarl/env_base.py
@@ -10,54 +10,58 @@
 # to permit others to do so.
 
 
-import json, os, sys
-from abc import ABC, abstractmethod
+import json, os, sys, gym, time
+#from abc import ABC, abstractmethod
+from gym import Wrapper
+from mpi4py import MPI
 
-class ExaEnv(ABC):
-    def __init__(self, **kwargs):        
+class ExaEnv(gym.Wrapper):
+    def __init__(self, env, env_comm, **kwargs):  
+        super(ExaEnv, self).__init__(env)     
         # Use relative path not absolute
         self.base_dir = os.path.dirname(__file__)
         print(self.base_dir)
+        #self.env = env
+        self.env_comm = env_comm
         
-    def set_config(self, env_data):
-        self.env_data = env_data
-        self.run_type = env_data['run_type']
-         
-       # Add any MPI parameters                                                                                       
+        self.env_cfg = os.path.join(self.base_dir, '../envs/env_vault/env_cfg/env_setup.json')
+        for key, value in kwargs.items():
+            if key == 'env_cfg':
+                self.env_cfg = value
+
+        with open(self.env_cfg) as json_file:
+            env_data = json.load(json_file)
+
+        ## TODO: Add any MPI parameters
         self.mpi_children_per_parent = int(env_data['mpi_children_per_parent']) if 'mpi_children_per_parent' in env_data.keys() else 0
 
-        # Add any OMP parameters
-        self.omp_thread = int(env_data['omp_thread']) if 'omp_thread' in env_data.keys() else 1   
-        
-        # Add any GPU parameters                                                                                                   
+        ## TODO: Add any OMP parameters
+        self.omp_thread = int(env_data['omp_thread']) if 'omp_thread' in env_data.keys() else 1
 
-        # Executable                                                                                                               
-        if(self.run_type == 'dynamic' and self.mpi_children_per_parent > 0):
-            self.worker = (env_data['worker_app']).lower() if 'worker_app' in env_data.keys() else print('Specify worker app')
+        ## TODO: Add any GPU parameters
+
+
+        ## TODO: executable 
+        if(self.mpi_children_per_parent > 0):
+            # defaults to running toy example of computing PI
+            self.worker = (env_data['worker_app']).lower() if 'worker_app' in env_data.keys() else "envs/env_vault/cpi.py"
         else:
             self.worker = None
+        
+    def set_results_dir(self,results_dir):
+        ''' 
+        Default method to save environment specific information 
+        '''
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+        ## Top level directory 
+        self.results_dir=results_dir
+
+    def set_config(self, env_data):
+        self.env_data = env_data
 
     def get_config(self):
         return self.env_data
         
-    @abstractmethod
-    def step(self, action):
-        ''' 
-        Required by all environment to be implemented by user 
-        '''
-        pass
-
-    @abstractmethod
-    def reset(self):
-        ''' 
-        Required by all environment to be implemented by user 
-        '''
-        pass
-
-    @abstractmethod
     def set_env(self):
-        '''
-        Required by all environment to be implemented by user.
-        This function is used to set all the hyper-parameters for the environment
-        '''
-        pass
+        print('Use this function to set hyper-parameters, if any')
