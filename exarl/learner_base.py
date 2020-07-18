0c39b5e82eabb3857dffec2ac652efeef72d7907
schr476 <malachi.schram@pnnl.gov>
2020-07-17 17:51:26 -0700

    clean up the learner base

--- exarl/learner_base.py
+++ exarl/learner_base.py
@@ -15,6 +15,8 @@ import gym
 import envs
 import agents
 
+from exarl.env_base import ExaEnv
+
 import os
 import csv
 
@@ -26,9 +28,16 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('RL-Logger')
 logger.setLevel(logging.INFO)
 
+import exarl.mpi_settings as mpi_settings
+
 class ExaLearner():
 
     def __init__(self, run_params):
+        # World communicator
+        self.world_comm = MPI.COMM_WORLD
+        world_rank = self.world_comm.rank
+        world_size = self.world_comm.size
+        
         # Default training
         self.nepisodes = 1
         self.nsteps = 10
@@ -37,6 +46,14 @@ class ExaLearner():
 
         self.run_type = run_params['run_type']
         self.mpi_children_per_parent = int(run_params['mpi_children_per_parent'])
+       
+        # Set configuration
+        self.mpi_children_per_parent = run_params['mpi_children_per_parent']
+        if self.world_comm.size/(self.mpi_children_per_parent+1)<1:
+            num_parents=1
+        else:    
+            num_parents=int(self.world_comm.size/(self.mpi_children_per_parent+1))
+        mpi_settings.init(num_parents*self.mpi_children_per_parent)
         # Setup agent and environments
         agent_id = 'agents:'+run_params['agent']
         env_id   = 'envs:'+run_params['env']
@@ -46,81 +63,16 @@ class ExaLearner():
         #self.env._max_episode_steps = self.nsteps
         self.env.spec.max_episode_steps  = self.nsteps
 
-        # Set configuration
-        self.mpi_children_per_parent = run_params['mpi_children_per_parent']
+        ##
         self.set_config(run_params)
 
+        
     def make(self):
-        # World communicator
-        self.world_comm = MPI.COMM_WORLD
-        world_rank = self.world_comm.rank
-        world_size = self.world_comm.size
-
-        if self.run_type == 'static-two-groups':
-            ### leaders: {0:worker_begin-1}, workers: {worker_begin, size-1}
-            self.leader = False 
-            self.worker_begin = int(world_size / self.mpi_children_per_parent)
-            if self.worker_begin == 0:
-                print('[Aborting] Worker and Leader cannot have the same rank. Increase #processes and try again.')
-                self.world_comm.Abort()
-            if world_rank < self.worker_begin:
-                self.leader = True 
-            ### Identify leader colors (assumes 0 is *always* a leader)
-            color = 2
-            if world_rank >= self.worker_begin:
-                color = 1
-            ### leaders(2) and workers(1) intracomm
-            self.intracomm = self.world_comm.Split(color, world_rank)
-            # TODO redundant
-            self.env_comm = self.intracomm
-            self.agent_comm = self.env_comm
-            env = gym.make(self.env_id, env_comm=self.env_comm)
-            agent = agents.make(self.agent_id, env=env, agent_comm=self.agent_comm)
-            # intercommunicator
-            # group 1 (worker) communicates with group 2 (leader)
-            if color == 1:
-                self.intercomm = MPI.Intracomm.Create_intercomm(self.intracomm, 0, self.world_comm, 0)
-            # group 2 (leader) communicates with group 1 (worker)
-            if color == 2:
-                self.intercomm = MPI.Intracomm.Create_intercomm(self.intracomm, 0, self.world_comm, self.worker_begin)
-        elif self.run_type == 'static-multi-groups':
-            ### Assumes 0 is *always* the leader of agents
-            self.leader = False 
-            ncolors = self.mpi_children_per_parent
-            color = int(world_rank % ncolors)
-            if color == 0: 
-                self.leader = True
-            # one-to-many group communication
-            self.intracomm = self.world_comm.Split(color, world_rank)
-            # TODO redundant
-            self.env_comm = self.intracomm
-            self.agent_comm = self.env_comm
-            env = gym.make(self.env_id, env_comm=self.env_comm)
-            agent = agents.make(self.agent_id, env=env, agent_comm=self.agent_comm)
-            # intercommunicators
-            self.intercomm = [MPI.COMM_NULL]*(ncolors-1)
-            if color == 0:
-                for i in range(ncolors-1):
-                    self.intercomm[i] = MPI.Intracomm.Create_intercomm(self.intracomm, 0, self.world_comm, i+1)
-            else:
-                self.intercomm[0] = MPI.Intracomm.Create_intercomm(self.intracomm, 0, self.world_comm, 0)
-        elif self.run_type == 'static-omp':
-            self.env_comm = MPI.COMM_SELF
-            self.agent_comm = MPI.COMM_WORLD
-
-            # Create environment object
-            env = gym.make(self.env_id)
-            agent = agents.make(self.agent_id, env=env)
-        else:
-            # Environment communicator
-            color = int(world_rank/(self.mpi_children_per_parent))#+1))
-            self.env_comm = self.world_comm.Split(color, world_rank)
-            self.agent_comm = self.env_comm
-
-            # Create environment object
-            env = gym.make(self.env_id, env_comm=self.env_comm)
-            agent = agents.make(self.agent_id, env=env, agent_comm=self.agent_comm)
-         
+        
+        # Create environment object
+        env = ExaEnv(gym.make(self.env_id))
+        agent = agents.make(self.agent_id, env=env)
+ 
         return agent, env
 
 
@@ -223,225 +175,7 @@ class ExaLearner():
 
         train_file.close()
         
-    ### Uses two intercomms for communicating agent comm with environment comms
-    def run_exarl_two_groups(self, is_leader, worker_begin, intracomm, intercomm):
-        rank0_memories = 0
-        target_weights = None
-        comm = MPI.COMM_WORLD
-        rank = comm.Get_rank()
-        size = comm.Get_size()
-        filename_prefix = 'ExaLearner_' + 'Episodes%s_Steps%s_Rank%s_memory_v1' % ( str(self.nepisodes), str(self.nsteps), str(rank))
-        if is_leader is False: # only workers will update
-            train_file = open(self.results_dir+'/'+filename_prefix + ".log", 'w')
-            train_writer = csv.writer(train_file, delimiter = " ")
-
-        start_time_episode = time.time()
-        steps = 0
-        for e in range(self.nepisodes):
-            current_state = self.env.reset()
-            total_reward=0
-            done = True # for leaders
-            if is_leader is False:
-                done = False
-            all_done = False
-            root = 0
-            if rank == 0:
-                root = MPI.ROOT # remote leader
-            if rank != 0 and is_leader == True:
-                root = MPI.PROC_NULL
-            
-            while all_done!=True:
-
-                worker_state = None
-                new_data = [] 
-                
-                ### workers
-                if is_leader is False:
-                    done = intracomm.allreduce(done, op=MPI.LAND)
-                    if done != True:
-                        action = self.agent.action(current_state)
-                        next_state, reward, done, _ = self.env.step(action)
-                        total_reward += reward
-                        worker_state = (action, reward, next_state, done, total_reward)
-               
-                ### communicate from workers to remote leader of workers                   
-                worker_state = intercomm.gather(worker_state, root=root)
-                
-                ## Learner (also a leader) ##
-                if rank == 0:
-                    for wdata in worker_state:
-                        if wdata is not None:
-                            new_data.append([current_state, wdata[0], wdata[1], wdata[2], wdata[3], wdata[4]])
-
-                    ## Push memories to learner ##
-                    if new_data is not None:
-                        for data in new_data:
-                            self.agent.remember(data[0],data[1],data[2],data[3],data[4])    
-                        self.agent.train()
-                        rank0_memories = len(self.agent.memory)
-                        target_weights = self.agent.get_weights()
-                        if rank0_memories%(intracomm.size) == 0:
-                            self.agent.save(self.results_dir+'/'+filename_prefix+'.h5')
-
-                ### communicate from remote leader to local leader of workers
-                ## Broadcast the memory size and the model weights to the workers  ##        
-                ## TODO only send to worker root
-                rank0_memories = intercomm.bcast(rank0_memories, root=root)
-                current_weights = intercomm.bcast(target_weights, root=root)
-                new_data = intercomm.bcast(new_data, root=root)
-                
-                ## Set the model weight for all the workers
-                if is_leader is False:
-                    if rank0_memories is not None and rank0_memories>30:                            
-                        self.agent.set_weights(current_weights)
-                    ## Update state
-                    if done != True:
-                        current_state = next_state
-                    ## Save memory for offline analysis
-                    # current_state,action,reward,next_state,total_reward,done
-                    if new_data is not None:
-                        for data in new_data:
-                            train_writer.writerow([current_state,data[1],data[2],data[3],data[5],data[4]])
-                            train_file.flush()
-                           
-                ## Save Learning target model
-                if rank == 0:
-                    self.agent.save(self.results_dir+'/'+filename_prefix+'.h5')
-
-                steps += 1
-                if steps >= self.nsteps:
-                    done = True
-
-                ## Exit criteria
-                all_done = comm.allreduce(done, op=MPI.LAND)
-          
-            end_time_episode = time.time()
-            mtim = end_time_episode - start_time_episode 
-            ptim = comm.reduce(mtim, op=MPI.SUM, root=0)
-            apes = intracomm.Get_size()
-            if rank == 0:
-                atim = ptim / size            
-                epes = size - apes
-                logger.info('Average execution time (in secs.) for %s episodes on %s agent processes and %s environment processes: %s ' %(str(self.nepisodes), str(apes), str(epes), str(ptim / size)))
-            if is_leader is False:
-                train_file.close()
  
-    ### Uses multiple intercomms for communicating agent comm with environment comms
-    def run_exarl_multi_groups(self, is_leader, ncolors, intracomm, intercomm):
-        rank0_memories = 0
-        target_weights = None
-        comm = MPI.COMM_WORLD
-        rank = comm.Get_rank()
-        size = comm.Get_size()
-        filename_prefix = 'ExaLearner_' + 'Episodes%s_Steps%s_Rank%s_memory_v1' % ( str(self.nepisodes), str(self.nsteps), str(rank))
-        if is_leader is False: # only workers will update
-            train_file = open(self.results_dir+'/'+filename_prefix + ".log", 'w')
-            train_writer = csv.writer(train_file, delimiter = " ")
-
-        start_time_episode = time.time()
-        steps = 0
-        for e in range(self.nepisodes):
-            current_state = self.env.reset()
-            total_reward=0
-            all_done = False
-            done = False
-            if is_leader is True: # for leaders
-                done = True
-            
-            root = 0
-            if is_leader is True: 
-                root = MPI.PROC_NULL
-            if rank == 0:
-                root = MPI.ROOT
-             
-            while all_done!=True:
-
-                new_data = [] 
-                worker_data = []
-                worker_state = None
-              
-                ### workers
-                if is_leader is False: 
-                    done = intracomm.allreduce(done, op=MPI.LAND)
-                    if done != True:
-                        action = self.agent.action(current_state)
-                        next_state, reward, done, _ = self.env.step(action)
-                        total_reward += reward
-                        worker_state = (action, reward, next_state, done, total_reward)
-
-                ### communicate from workers to remote leader of workers
-                if is_leader is True: 
-                    for i in range(ncolors-1):
-                        worker_data = intercomm[i].gather(worker_state, root=root)
-                else:
-                    worker_data = intercomm[0].gather(worker_state, root=root)
-                
-                ## Learner (also a leader) ##
-                if rank == 0:
-                    for wdata in worker_data:
-                        if wdata is not None:
-                            new_data.append([current_state, wdata[0], wdata[1], wdata[2], wdata[3], wdata[4]])
-                        if new_data is not None:
-                            for data in new_data:
-                                self.agent.remember(data[0],data[1],data[2],data[3],data[4])
-                            self.agent.train()
-                            rank0_memories = len(self.agent.memory)
-                            target_weights = self.agent.get_weights()
-           
-                ### communicate from remote leader to local leader of workers
-                ## broadcast the memory size and the model weights to the workers  ##       
-                if is_leader is True: 
-                    for i in range(ncolors-1):
-                        rank0_memories = intercomm[i].bcast(rank0_memories, root=root)
-                else:
-                    rank0_memories = intercomm[0].bcast(rank0_memories, root=root)
-                
-                if is_leader is True: 
-                    for i in range(ncolors-1):
-                        current_weights = intercomm[i].bcast(target_weights, root=root)
-                else:
-                    current_weights = intercomm[0].bcast(target_weights, root=root)
-                
-                if is_leader is True: 
-                    for i in range(ncolors-1):
-                        new_data = intercomm[i].bcast(new_data, root=root)
-                else:
-                    new_data = intercomm[0].bcast(new_data, root=root)       
- 
-                ## Set the model weight for all the workers
-                if is_leader is False: 
-                    if current_weights is not None and rank0_memories is not None and rank0_memories>30:                            
-                        self.agent.set_weights(current_weights)
-
-                    ## Update state
-                    if done != True:
-                        current_state = next_state
-                    
-                    ## Save memory for offline analysis
-                    # current_state,action,reward,next_state,total_reward,done
-                    if new_data is not None:
-                        for data in new_data:
-                            train_writer.writerow([current_state,data[1],data[2],data[3],data[5],data[4]])
-                            train_file.flush()
-                           
-                steps += 1
-                if steps >= self.nsteps:
-                    done = True
-
-                ## Exit criteria
-                all_done = comm.allreduce(done, op=MPI.LAND)
-            
-            end_time_episode = time.time()
-            mtim = end_time_episode - start_time_episode 
-            ptim = comm.reduce(mtim, op=MPI.SUM, root=0)
-            apes = intracomm.Get_size()
-            if rank == 0:
-                atim = ptim / size            
-                epes = size - apes
-                logger.info('Average execution time (in secs.) for %s episodes on %s agent processes and %s environment processes (%s groups): %s ' %(str(self.nepisodes), str(apes), str(epes), str(ncolors-1), str(ptim / size)))
-            if is_leader is False:
-                train_file.close()
-
     def run(self, run_type):
         if self.agent!=None:
             self.agent.set_agent()
@@ -449,33 +183,8 @@ class ExaLearner():
         if self.env!=None:
             self.env.set_env()
 
-        if run_type == 'static-two-groups':
-            self.run_exarl_two_groups(self.leader, self.worker_begin, self.intracomm, self.intercomm)
-            self.intracomm.Free()
-            self.intercomm.Free()
-        elif run_type == 'static-multi-groups':
-            ncolors = int(self.mpi_children_per_parent)
-            self.run_exarl_multi_groups(self.leader, ncolors, self.intracomm, self.intercomm)
-            self.intracomm.Free()
-            if self.leader is True: 
-                for i in range(ncolors-1):
-                    self.intercomm[i].Free()
-            else:
-                self.intercomm[0].Free()
-            if self.env_comm != MPI.COMM_NULL:
-                self.env_comm.Free()
-        elif run_type == 'static-omp':
-            # TODO add self.omp_num_threads as a param, override
-            # with OMP_NUM_THREADS
-            #os.environ['OMP_NUM_THREADS']='{:d}'.format(self.omp_num_threads)
-            self.run_exarl(self.agent_comm)
-        elif run_type == 'dynamic':
-            self.run_exarl(self.world_comm)
-            if self.env_comm != MPI.COMM_NULL:
-                self.env_comm.Free()
-        else:
-            if self.agent_comm != MPI.COMM_NULL:
-                self.run_exarl(self.agent_comm)
-                self.agent_comm.Free()
-            if self.env_comm != MPI.COMM_NULL:
-                self.env_comm.Free()
+        # TODO add self.omp_num_threads as a param, override
+        # with OMP_NUM_THREADS
+        #os.environ['OMP_NUM_THREADS']='{:d}'.format(self.omp_num_threads)
+        self.run_exarl(mpi_settings.agent_comm)
+        
