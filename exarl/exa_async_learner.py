87fbe9f7141bd05b7cb3138641e4320578e8c7d2
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2020-08-09 14:46:23 -0600

    bug fixes in async learner

--- exarl/exa_async_learner.py
+++ exarl/exa_async_learner.py
@@ -22,19 +22,19 @@ def run_async_learner(self, comm):
         self.agent.set_weights(current_weights)
 
         # Variables for all
+        episode = 0
         rank0_memories = 0
         rank0_epsilon  = 0
 
         ## Round-Robin Scheduler
-        if rank == 0:
+        if comm.rank == 0:
                 while 1:
                         if episode < self.nepisodes:
                                 print("Running scheduler/learner")
                                 done = False
                                 while done != True: 
                                         # Receive batch
-                                        req = comm.Irecv(recv_data, source=MPI.ANY_SOURCE)
-                                        req.Wait()
+                                        recv_data = comm.recv(source=MPI.ANY_SOURCE)
                                         whofrom = recv_data[0]
                                         batch = recv_data[1]
                                         done = recv_data[2]
@@ -42,24 +42,24 @@ def run_async_learner(self, comm):
                                         # Train
                                         self.agent.train(batch)
                                         self.agent.target_train()
-                                        rank0_epsilon = self.target.epsilon
+                                        rank0_epsilon = self.agent.epsilon
                                         target_weights =self.agent.get_weights()
                                         
                                         # Send target weights
-                                        comm.Isend([rank0_epsilon, target_weights], source = whofrom)
+                                        comm.send([episode, rank0_epsilon, target_weights], dest = whofrom)
                                 
                                 # Increment episode
                                 episode += 1
                         else:
                                 print("Finishing up ...")
-                                episode[0] = -1
+                                episode = -1
                                 for s in range(1, comm.Get_size()):
-                                        comm.Isend(episode, dest=s)
+                                        comm.send([episode, 0], dest=s)
                                 break
 
         else:                   
                 # Setup logger
-                filename_prefix = 'ExaLearner_' + 'Episodes%s_Steps%s_Rank%s_memory_v1' 
+                filename_prefix = 'ExaLearner_' + 'Episodes%s_Steps%s_Rank%s_memory_v1' \
                                   % ( str(self.nepisodes), str(self.nsteps), str(comm.rank))
                 train_file = open(self.results_dir+'/'+filename_prefix + ".log", 'w')
                 train_writer = csv.writer(train_file, delimiter = " ")
@@ -71,13 +71,13 @@ def run_async_learner(self, comm):
                 done         = False
                 all_done     = False
 
-                while episode[0] != -1:
+                while episode != -1:
 
                         # Steps in an episode
                         for i in range(self.nsteps):
-                                action = self.agent.action(current_state)
+                                action, policy_type = self.agent.action(current_state)
                                 next_state, reward, done, _ = self.env.step(action)
-                                total_reward+=reward
+                                total_reward += reward
                                 memory = (current_state, action, reward, next_state, done, total_reward)
                                 
                                 batch_data = []
@@ -88,31 +88,29 @@ def run_async_learner(self, comm):
                                 logger.info('Rank[{}] - Memories: {}'.format(comm.rank,len(self.agent.memory)))
 
                                 # Send batched memories
-                                req = comm.Isend([comm.rank, batch_data, done], dest=0)
+                                comm.send([comm.rank, batch_data, done], dest=0)
                                 
                                 # Receive target weights
-                                req = comm.Irecv(recv_data, source=0)
-                                req.Wait()
+                                recv_data = comm.recv(source=0)
+                                episode = recv_data[0]
+                                if episode == -1:
+                                        break
 
-                                self.agent.epsilon = recv_data[0]
-                                self.agent.set_weights(recv_data[1])
+                                self.agent.epsilon = recv_data[1]
+                                self.agent.set_weights(recv_data[2])
                                 
                                 # Update state
                                 current_state = next_state
                                 logger.info('Rank[%s] - Total Reward:%s' % (str(comm.rank),str(total_reward)))
 
-                                train_writer.writerow([time.time(),current_state,action,reward,next_state,total_reward,
-                                                       done, e, steps, policy_type, rank0_epsilon])
+                                train_writer.writerow([time.time(),current_state,action,reward,next_state,total_reward, \
+                                                       done, steps, policy_type, rank0_epsilon])
                                 train_file.flush()
 
                                 # Break loop if done
                                 if done:
                                         break
-                        
-                        logger.info('Rank[%s] run-time for episode %s: %s ' 
-                                    % (str(comm.rank), str(e), str(end_time_episode - start_time_episode)))
-                        logger.info('Rank[%s] run-time for episode per step %s: %s '
-                                    % (str(comm.rank), str(e), str((end_time_episode - start_time_episode)/steps)))
+
 
                 train_file.close()
                                 
