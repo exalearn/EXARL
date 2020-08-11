16a4f9f96ba721ed35b0707c433305c0438fcacc
Jamal <jamal@lanl.gov>
2020-08-11 17:51:53 -0600

    Only rank 0 writes output_dir

--- candlelib/default_utils.py
+++ candlelib/default_utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 from pprint import pprint
 import inspect
+from mpi4py import MPI
 
 import logging
 
@@ -651,20 +652,26 @@ def directory_from_parameters(params, commonroot='Output'):
 
     """
 
+    world_comm = MPI.COMM_WORLD
+    myrank = world_comm.rank
+
     if commonroot in set(['.', './']): # Same directory --> convert to absolute path
         outdir = os.path.abspath('.')
     else: # Create path specified
         outdir = os.path.abspath(os.path.join('.', commonroot))
         if not os.path.exists(outdir):
-            os.makedirs(outdir)
+            if (myrank == 0):
+                os.makedirs(outdir)
 
         outdir = os.path.abspath(os.path.join(outdir, params['experiment_id']))
         if not os.path.exists(outdir):
-            os.makedirs(outdir)
+            if (myrank == 0):
+                os.makedirs(outdir)
 
         outdir = os.path.abspath(os.path.join(outdir, params['run_id']))
         if not os.path.exists(outdir):
-            os.makedirs(outdir)
+            if (myrank == 0):
+                os.makedirs(outdir)
 
 
     return outdir
