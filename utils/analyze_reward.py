36377526d4677df57dc9676e1d7c8876556987e3
rvinaybharadwaj <vinayr@lanl.gov>
2020-08-28 14:52:45 -0400

    bug fix in plot code:label

--- utils/analyze_reward.py
+++ utils/analyze_reward.py
@@ -31,13 +31,12 @@ def save_reward_plot(results_dir):
             df_ranks.append(df)
             print(df.head())
 
-    # df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['time'],how='outer'), df_ranks)
+
     df_merged = pd.concat(df_ranks)
     df_merged.set_index('episode', inplace=True)
     df_merged = df_merged.reset_index()
     fig,ax = plt.subplots(figsize=(14,12))
-    #ax.plot(df_merged['total_reward'], label='Total Reward', color='blue')
-    ax = sns.scatterplot(x="rel_time", y="total_reward_roll", data=df_merged)
+    ax = sns.scatterplot(x="rel_time", y="total_reward", data=df_merged)
     plt.xlabel('Time')
     plt.ylabel('Total Reward')
     if not os.path.exists(results_dir+'/Plots'):
