ac845fa48ec8a714b124392c29f852cd11205b50
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2020-09-08 18:25:02 -0600

    adding missing config files

--- driver/candleDriver.py
+++ driver/candleDriver.py
@@ -11,34 +11,6 @@ from pprint import pprint
 import json
 import argparse
 
-'''
-# These are just added to the command line options
-additional_definitions =  [
-# learner params
-{'name':'n_episodes', 'type':int,'help':'Number of episodes to run'},
-{'name':'n_steps', 'type':int,'help':'Number of steps to run'},
-{'name':'agent', 'type':int,'help':'agent'},
-{'name':'env', 'type':int,'help':'environment'},
-{'name':'run_type', 'type':str, 'choices':['static','dynamic'],'help':'run_type'},
-# agent params
-#{'name':'search_method',  'default':'epsilon', 'help':'Search method'},
-#{'name':'gamma', 'type':float, 'default': 0.95,'help':'discount rate'},
-#{'name':'epsilon', 'type':float,'default': 1.0  ,'help':'exploration rate'},
-#{'name':'epsilon_min', 'type':float,'default': 0.05},
-#{'name':'epsilon_decay', 'type':float, 'default': 0.995},
-#{'name':'learning_rate', 'type':float, 'default':  0.001},
-#{'name':'batch_size', 'type':int,'default': 32},
-#{'name':'tau', 'type':float,'default': 0.5}
-{'name':'search_method', 'type':str, 'help':'Search method'},
-{'name':'gamma', 'type':float, 'help':'discount rate'},
-{'name':'epsilon', 'type':float, 'help':'exploration rate'},
-{'name':'epsilon_min', 'type':float},
-{'name':'epsilon_decay', 'type':float},
-{'name':'learning_rate', 'type':float},
-{'name':'batch_size', 'type':int},
-{'name':'tau', 'type':float}
-]
-'''
 
 #required = ['agent', 'env', 'n_episodes', 'n_steps']
 required = ['agent', 'env']
