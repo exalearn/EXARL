1059a18026740c33df6ae8c97abe7608a09dd79c
Schram <schr476@we40963.pnl.gov>
2020-09-15 10:17:19 -0700

    Update the pydemic env

--- envs/env_vault/ExaCOVID.py
+++ envs/env_vault/ExaCOVID.py
@@ -6,9 +6,7 @@ import json
 import exarl as erl
 import numpy as np
 import pandas as pd
-import os, sys
-## Load pydemic module ##
-sys.path.append(os.path.dirname(__file__)+'/pydemic/')
+
 from pydemic.models import SEIRPlusPlusSimulation
 from pydemic import MitigationModel
 
@@ -37,13 +35,23 @@ class ExaCOVID(gym.Env):
         """
         #self.cfg_data = super.get_config()
 
-        ##
+        self.icu_max=100
+
+        ''' Mitigation factor is used as the action '''
+        self.factor_init = 1
+        self.factor_final = 1
+
+        ''' Define the model time scale for each step '''
+        self.time_init = 30 # [days] a month delay
+        self.mitigation_dt = 7 # [days]
+        self.time_final = self.time_init+self.mitigation_dt
+
+        ''' Define the initial model parameters and distributions '''
         state = "Illinois"
         from pydemic.data.united_states import nyt, get_population, get_age_distribution
         self.data = nyt(state)
         self.total_population = get_population(state)
         self.age_distribution = get_age_distribution()
-
         self.tspan = ('2020-02-15', '2020-05-30')
 
         from pydemic.distributions import GammaDistribution
@@ -71,74 +79,73 @@ class ExaCOVID(gym.Env):
             all_dead_dist=GammaDistribution(3, 3),
             all_dead_multiplier=1.,
         )
-        t0, tf = 50, 140
-        times = [70, 80]
-        factors = [1, .48]
-        self.mitigation = MitigationModel(t0, tf, times, factors)
 
-        ## Define state and action spaces
-        self.policies_len  =1 ## Only using workfromhome policy for now
-        self.workfraction_step =0.1
-        self.structure_len = 1
-        self.observation_space = spaces.Box(low=np.append(np.zeros(self.structure_len),[0.004]), high=np.append(np.ones(self.structure_len)*350,[0.012]),dtype=np.float32)
+        ''' Observation state based on the seirpp.py dict
+            increment_keys = ('infected', 'dead', 'all_dead', 'positive',
+                      'admitted_to_hospital', 'total_discharged')
+        '''
+        self.result = {}
+        self.observation_space = spaces.Box(low=np.append(np.zeros(6)),
+                                            high=np.append(np.ones(self.structure_len)*self.total_population),
+                                            dtype=np.float32)
 
         ## Increase, Decrease, Don't change
-        self.action_space = spaces.Discrete(3) 
-
-        ## Create model parameter ##
-        self.model_parameter = {}
+        self.action_space = spaces.Discrete(3)
+        self.action_mul = 0.01
 
     def step(self, action):
-        ## Initial step variable ##
+
+        ''' Initial step variables '''
         done   = False
         reward = 0
         info   = ''
-        
-        # ## Apply discrete actions
-        # if action==1:
-        #     self.model_parameter['workfromhome']+=self.workfraction_step
-        #     if self.model_parameter['workfromhome']>self.observation_space.high[0]:
-        #         self.model_parameter['workfromhome']-=self.workfraction_step
-        #         done = True
-        #         return self._getState(),reward,done, {}
-        #
-        # elif action==2:
-        #     self.model_parameter['workfromhome']-=self.workfraction_step
-        #     if self.model_parameter['workfromhome']<self.observation_space.high[0]:
-        #         self.model_parameter['workfromhome']+=self.workfraction_step
-        #         done = True
-        #         return self._getState(),reward,done, {}
+
+        self.time_init = self.time_final
+        self.time_final = self.time_init+self.mitigation_dt
+        self.factor_init = self.factor_final
+
+        ''' Apply discrete actions '''
+        if action==1:
+            self.factor_final = self.factor_final * action_mul
+        elif action == 2:
+            self.factor_final = self.factor_final / action_mul
+
+        ''' Out of bounds'''
+        if self.factor_final>1:
+            done = True
+            reward = -99
+            info = 'Out of bounds (upper)'
+
+        if self.factor_final < 0:
+            done = True
+            reward = -99
+            info = 'Out of bounds (lower)'
 
         ## Make input file ##
-            
+        t0, tf = 0, 12*self.mitigation_dt ## TODO: What range should consider ?? ##
+        times = [self.time_init, self.time_final]  ## days from start (2020/1/1) -- to be defined by step counter
+        factors = [self.factor_init, self.factor_final] ## To be optimized
+        mitigation = MitigationModel(t0, tf, times, factors)
+
         ## Run model state ##
         sim = SEIRPlusPlusSimulation(self.total_population, self.age_distribution,
                                      mitigation=self.mitigation, **self.parameters)
-        
-        ## Calculate the number of infected ##
-        #reward = self.__get_reward(filename)
- 
-        
+
+        ''' Run simulation and results (dict) '''
+        self.result = self.sim(tspan, y0, .05)
+        if self.result.y['icu']>self.icu_max:
+            reward = -999
+            done = True
+            info = 'Exceeded the ICU capacity'
+
+        ''' Convert dict to state array '''
         return next_state, reward, done, info
     
     def __get_reward(self,filename):
         ''' Description:
              Calculates the total number of symptomatic people 
         '''
-        reward        = 0
-        total_people  = 0
-        total_healthy = 0
-        total_symp    = 0
-        f = open(filename, "r")
-        for x in f:
-            if 'People:' in x:
-                total_people = int(x.split()[-1])
-            if 'Total symptomatic individuals by age:' in x:
-                total_symp = sum( int(x) for x in ((x.split())[-1].split(','))[0:-1])
 
-        total_healthy = total_people - total_symp
-        if total_people!=0 and total_healthy>0:
-            reward = total_healthy/total_people
             
         return reward
     
