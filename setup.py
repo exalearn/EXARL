93ded8303bd7a4397453707640f3b41ba8fd06f9
Joshua Suetterlein <joshua.suetterlein@pnnl.gov>
2020-11-12 14:45:51 -0800

    Attempting to cleanup installation using conda..

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
-setup(name='exarl',
-      version='0.0.1',
-      install_requires=['ase','Lmfit','keras','mpi4py','seaborn'] # dependencies #gym
-)
+setup(name='exarl', 
+      version='0.0.1'
+      description='A scalable software framework for reinforcement learning environments and agents/policies used for the Design and Control applications',
+      url='https://github.com/exalearn/ExaRL/',
+      license='BSD-3',
+      packages=['exarl', 'agents', 'envs'],
+      zip_safe=False)
