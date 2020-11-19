74f5e5d9036149c7c2f5d6cf53015eb52bbf42fd
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2020-11-19 14:34:41 -0700

    Changing env name in env_vault __init__

--- envs/env_vault/__init__.py
+++ envs/env_vault/__init__.py
@@ -3,15 +3,15 @@ from utils.candleDriver import initialize_parameters
 run_params = initialize_parameters()
 env = run_params['env']
 
-if env == 'ExaLearnBlockCoPolymerTDLG-v0':
+if env == 'ExaTDLG-v0':
     from envs.env_vault.exalearn_bcp_tdlg import BlockCoPolymerTDLG
-elif env == 'ExaLearnBlockCoPolymerTDLG-v3':
+elif env == 'ExaTDLG-v3':
     from envs.env_vault.exalearn_bcp_tdlg_v3 import BlockCoPolymerTDLGv3
-elif env == 'ExaLearnCartpole-v0':
+elif env == 'ExaCartPole-v0':
     from envs.env_vault.ExaCartpoleDynamic import ExaCartpoleDynamic
-elif env == 'ExaLearnCartpole-v1':
+elif env == 'ExaCartPole-v1':
     from envs.env_vault.ExaCartpoleStatic import ExaCartpoleStatic
-elif env == 'ch-v0':
+elif env == 'ExaCH-v0':
     from envs.env_vault.env_ch import CahnHilliardEnv
 elif env == 'ExaCovid-v0':
     from envs.env_vault.ExaCOVID import ExaCOVID
