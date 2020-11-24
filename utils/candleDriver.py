c3c333855bb86f34c8bfdf5af7a153929730d9f6
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2020-11-24 09:16:41 -0700

    bug fix in candlrDriver

--- utils/candleDriver.py
+++ utils/candleDriver.py
@@ -2,14 +2,13 @@ import argparse
 import json
 import utils.log as log
 from pprint import pformat
-import candle
 import keras
 import os
 import sys
 file_path = os.path.dirname(os.path.realpath(__file__))
 lib_path2 = os.path.abspath(os.path.join(file_path, '..', 'candlelib'))
 sys.path.append(lib_path2)
-
+import candle
 # from pprint import pprint
 
 
