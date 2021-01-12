41e1239757b95e27b499029e28d8f66ffe5d318f
Schram <malachi.schram@pnnl.gov>
2021-01-11 18:50:03 -0800

    Remove problematic file

--- prep_data.py
+++
@@ -1,9 +0,0 @@
-import pickle as pk
-import numpy as np
-
-f = open('target_weights', 'rb')
-target_weights = np.array(pk.load(f), dtype='f')
-f.close()
-
-for i in range(np.size(target_weights)):
-    target_weights[i].flatten()
