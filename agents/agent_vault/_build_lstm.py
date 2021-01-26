47cfeb179b0a2850e25516bac578e79f2453c069
Malachi Schram <schr476@login4.summit.olcf.ornl.gov>
2021-01-26 15:34:05 -0500

    Update to include MirroredStrategy

--- agents/agent_vault/_build_lstm.py
+++ agents/agent_vault/_build_lstm.py
@@ -1,7 +1,7 @@
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input, GaussianNoise, BatchNormalization, LSTM
-from keras.optimizers import Adam
-from keras.regularizers import l1, l2, l1_l2
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, Input, GaussianNoise, BatchNormalization, LSTM
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l1, l2, l1_l2
 
 
 def build_model(self):
@@ -13,20 +13,20 @@ def build_model(self):
     model.add(LSTM(self.lstm_layers[0], activation=self.activation,
                    return_sequences=True, input_shape=(1, self.env.observation_space.shape[0])))
     model.add(BatchNormalization())
-    model.add(GaussianNoise(self.gauss_noise[0]))
+    model.add(Dropout(self.gauss_noise[0]))
 
     # loop over inner layers only
     for l in range(1, num_layers - 1):
         model.add(LSTM(self.lstm_layers[l], activation=self.activation,
                        return_sequences=True))
-        model.add(GaussianNoise(self.gauss_noise[l]))
+        model.add(Dropout(self.gauss_noise[l]))
 
     # special case for output layer
     l = num_layers = 1
     model.add(LSTM(self.lstm_layers[l], activation=self.activation,
                    kernel_regularizer=l1_l2(self.regularizer[0], self.regularizer[1]),
                    ))
-    model.add(GaussianNoise(self.gauss_noise[l]))
+    model.add(Dropout(self.gauss_noise[l]))
     model.add(Dense(self.env.action_space.n, activation=self.out_activation))
     opt = Adam(lr=1e-3)  # ,clipnorm=1.0, clipvalue=0.5)
     # opt = self.candle.build_optimizer(self.optimizer, self.learning_rate,
