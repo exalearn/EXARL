2e2a3953ec1743f9803413e05de2f5199efc81a5
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2021-02-09 13:14:26 -0700

    learner with flag

--- workflows/workflow_vault/rma_learner.py
+++ workflows/workflow_vault/rma_learner.py
@@ -71,11 +71,13 @@ class RMA_ASYNC(erl.ExaWorkflow):
 
         # Learner
         if mpi_settings.is_learner():
+
             # Initialize batch data buffer
             data_buffer = bytearray(serial_agent_batch_size)
             episode_count_learner = np.zeros(1, dtype=np.float64)
             epsilon = np.array(workflow.agent.epsilon, dtype=np.float64)
             learner_counter = 0
+            flag = np.zeros(agent_comm.size - 1)
             # Initialize epsilon
             epsilon_win.Lock(0)
             epsilon_win.Put(epsilon, target_rank=0)
@@ -94,8 +96,8 @@ class RMA_ASYNC(erl.ExaWorkflow):
                 episode_win.Unlock(0)
 
                 # Go over all actors (actor processes start from rank 1)
-                # s = (learner_counter % (agent_comm.size - 1)) + 1
-                s = np.random.randint(low=1, high=agent_comm.size, size=1)
+                s = (learner_counter % (agent_comm.size - 1)) + 1
+                # s = np.random.randint(low=1, high=agent_comm.size, size=1)
                 # Get data
                 data_win.Lock(s)
                 data_win.Get(data_buffer, target_rank=s, target=None)
@@ -105,9 +107,12 @@ class RMA_ASYNC(erl.ExaWorkflow):
                 try:
                     agent_data = MPI.pickle.loads(data_buffer)
                     data_counter += 1
+                    flag[s - 1] += 1
                 except Exception as e:
                     data_error_counter += 1
-                    print('In learner - problem with agent_data: '+ str(e))
+                    # print('In learner - problem with agent_data: ' + str(e))
+                    if flag[s - 1] > 0:
+                        print("Error after good stuff in rank = ", s)
                     continue
 
                 # print('***************************')
@@ -123,6 +128,7 @@ class RMA_ASYNC(erl.ExaWorkflow):
                 model_win.Unlock(0)
                 learner_counter += 1
 
+            print('flag = ', flag)
             print('data counter = ', data_counter)
             print('data error counter = ', data_error_counter)
             logger.info('Learner exit on rank_episode: {}_{}'.format(agent_comm.rank, episode_data))
@@ -223,17 +229,17 @@ class RMA_ASYNC(erl.ExaWorkflow):
                         memory = (current_state, action, reward, next_state, done, total_rewards)
                         workflow.agent.remember(memory[0], memory[1], memory[2], memory[3], memory[4])
                         batch_data = next(workflow.agent.generate_data())
-                        
+
                         try:
                             serial_agent_batch = (MPI.pickle.dumps(batch_data))
-                            print('actor serial data length:', len(serial_agent_batch))
+                            # print('actor serial data length:', len(serial_agent_batch))
                         except Exception as e:
-                            print('In actor - problem with agent_data: '+ str(e))
-                            
-                        # print('****************************')
-                        # print('batch data = ', batch_data)
+                            print('In actor - problem with agent_data: ' + str(e))
 
-                        # Write to data window
+                            # print('****************************')
+                            # print('batch data = ', batch_data)
+
+                            # Write to data window
                         serial_agent_batch = MPI.pickle.dumps(batch_data)
                         data_win.Lock(agent_comm.rank)
                         data_win.Put(serial_agent_batch, target_rank=agent_comm.rank)
