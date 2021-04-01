82cff0aeeaf7239e247ccbdeeb738ec2f7b1fbea
rvinaybharadwaj <vinayr@lanl.gov>
2021-04-01 16:29:46 -0600

    Update _build_lstm.py

--- agents/agent_vault/_build_lstm.py
+++ agents/agent_vault/_build_lstm.py
@@ -33,26 +33,22 @@ def build_model(self):
     model.add(LSTM(self.lstm_layers[0], activation=self.activation,
                    return_sequences=True, input_shape=(1, self.env.observation_space.shape[0])))
     model.add(BatchNormalization())
-    model.add(GaussianNoise(self.gauss_noise[0]))
+    model.add(Dropout(self.gauss_noise[0]))
 
     # loop over inner layers only
     for l in range(1, num_layers - 1):
         model.add(LSTM(self.lstm_layers[l], activation=self.activation,
                        return_sequences=True))
-        model.add(GaussianNoise(self.gauss_noise[l]))
+        model.add(Dropout(self.gauss_noise[l]))
 
     # special case for output layer
     l = num_layers = 1
     model.add(LSTM(self.lstm_layers[l], activation=self.activation,
                    kernel_regularizer=l1_l2(self.regularizer[0], self.regularizer[1]),
                    ))
-    model.add(GaussianNoise(self.gauss_noise[l]))
+    model.add(Dropout(self.gauss_noise[l]))
     model.add(Dense(self.env.action_space.n, activation=self.out_activation))
-    opt = Adam(lr=1e-3)  # ,clipnorm=1.0, clipvalue=0.5)
-    # opt = self.candle.build_optimizer(self.optimizer, self.learning_rate,
-    #                                  #clipnorm= self.clipnorm, clipvalue = self.clipvalue,
-    #                                  self.candle.keras_default_config())
-
-    # model.compile(loss=self.loss, optimizer=opt)
+    opt = Adam(lr=self.learning_rate)
+    
     # model.summary()
     return model
