1ae8f6bfe74da7d258e87d422b8cc7b95d7f9840
schr476 <48308123+schr476@users.noreply.github.com>
2021-04-01 15:34:57 -0700

    Update _build_mlp.py

--- agents/agent_vault/_build_mlp.py
+++ agents/agent_vault/_build_mlp.py
@@ -34,7 +34,7 @@ def build_model(self):
         layer_width = self.dense[i]
         layers.append(Dense(layer_width, activation=self.activation)(layers[-1]))
     # output layer
-    layers.append(Dense(self.env.action_space.n, activation=self.activation)(layers[-1]))
+    layers.append(Dense(self.env.action_space.n, activation=self.out_activation)(layers[-1]))
     layers.append(Flatten()(layers[-1]))
 
     model = Model(inputs=layers[0], outputs=layers[-1])
