44572157569e2b55b78fe531228d2ea4ce34cdab
schr476 <48308123+schr476@users.noreply.github.com>
2021-04-02 09:41:58 -0700

    Update __init__.py

--- envs/env_vault/__init__.py
+++ envs/env_vault/__init__.py
@@ -10,11 +10,7 @@ elif env == 'ExaCH-v0':
     from envs.env_vault.env_ch import CahnHilliardEnv
 elif env == 'ExaCovid-v0':
     from envs.env_vault.ExaCOVID import ExaCOVID
-elif env == 'ExaBooster-v1':
+elif env == 'ExaBoosterDiscrete-v0':
     from envs.env_vault.ExaBoosterDiscrete import ExaBooster_v1 as ExaBooster
-elif env == 'ExaBoosterContinuous-v1':
-    from envs.env_vault.surrogate_accelerator_v2 import Surrogate_Accelerator_v2 as ExaBoosterContinuous
-elif env == 'ExaLAMMPS-v0':
-    from envs.env_vault.exalearn_lammps_ex1 import ExaLammpsEx1 as ExaLAMMPS
-elif env == 'ExaWaterCluster-v0':
+elif env == 'ExaWaterClusterDiscrete-v0':
     from envs.env_vault.ExaWaterClusterDiscrete import ExaWaterClusterDiscrete
