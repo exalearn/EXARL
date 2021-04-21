c96911eb17566c6334660bfd9cdfe32cb9d08358
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2021-04-21 12:27:20 -0600

    adding priority replay class

---
+++ agents/agent_vault/_prioritized_replay.py
@@ -0,0 +1,36 @@
+import random
+import numpy as np
+import tensorflow as tf
+from collections import deque
+
+
+class PrioritizedReplayBuffer():
+    def __init__(self, maxlen):
+        self.buffer = deque(maxlen=maxlen)
+        self.priorities = deque(maxlen=maxlen)
+
+    def add(self, experience):
+        self.buffer.append(experience)
+        self.priorities.append(max(self.priorities, default=1))
+
+    def get_probabilities(self, priority_scale):
+        scaled_priorities = np.array(self.priorities) ** priority_scale
+        sample_probabilities = scaled_priorities / sum(scaled_priorities)
+        return sample_probabilities
+
+    def get_importance(self, probabilities):
+        importance = 1 / len(self.buffer) * 1 / probabilities
+        importance_normalized = importance / max(importance)
+        return importance_normalized
+
+    def sample(self, batch_size, priority_scale=1.0):
+        sample_size = min(len(self.buffer), batch_size)
+        sample_probs = self.get_probabilities(priority_scale)
+        sample_indices = random.choices(range(len(self.buffer)), k=sample_size, weights=sample_probs)
+        samples = np.array(self.buffer)[sample_indices]
+        importance = self.get_importance(sample_probs[sample_indices])
+        return map(list, zip(*samples)), importance, sample_indices
+
+    def set_priorities(self, indices, errors, offset=0.1):
+        for i, e in zip(indices, errors):
+            self.priorities[i] = abs(e) + offset
