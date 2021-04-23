3f1caaa8f1ef0c23153e1ef015dcf17aac35f9b7
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2021-04-23 12:52:32 -0600

    Restore CI, break job into unit and integration

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='exarl',
       version='0.0.1',
       description='ExaRL is a high performance reinforcement learning framework',
-      install_requires=['tensorflow-gpu>=2.0.0', 'keras', 'mpi4py', 'gym', 'ase', 'Lmfit', 'seaborn', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest']
+      install_requires=['keras', 'mpi4py', 'gym', 'ase', 'Lmfit', 'seaborn', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest']
       )
