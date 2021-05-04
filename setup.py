58ab428a00614594384ffa9574f20a8e7b10dd68
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2021-05-04 09:25:31 -0600

    Revert setup.py to develop branch

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='exarl',
       version='0.0.1',
       description='ExaRL is a high performance reinforcement learning framework',
-      install_requires=['keras', 'mpi4py', 'gym', 'ase', 'Lmfit', 'seaborn', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest'],
-      packages=find_packages(),
+      install_requires=['tensorflow-gpu>=2.0.0', 'keras', 'mpi4py', 'gym', 'ase', 'Lmfit', 'seaborn', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest']
       )
