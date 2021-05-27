be4ea2b763a646c3c2964c82b54b24143a2fb2c9
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2021-05-27 09:05:38 -0600

    adding horovod to list of dependencies in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name='exarl',
       version='0.0.1',
       description='ExaRL is a high performance reinforcement learning framework',
-      install_requires=['tensorflow-gpu>=2.0.0', 'keras', 'mpi4py', 'gym', 'ase', 'Lmfit', 'seaborn', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest']
+      install_requires=['tensorflow-gpu>=2.0.0', 'keras', 'mpi4py', 'horovod', 'gym', 'ase',
+                        'Lmfit', 'seaborn', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest']
       )
