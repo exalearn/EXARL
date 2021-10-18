694a1a6724ffdb0597575dbd26c354a8170fab76
Ted C Fujimoto <ted.fujimoto@pnnl.gov>
2021-10-18 11:11:22 -0700

    added conditional on state size

--- exarl/envs/env_vault/ExaBsuite.py
+++ exarl/envs/env_vault/ExaBsuite.py
@@ -38,26 +38,42 @@ class ExaBsuite(gym.Env):
 	def __init__(self) -> None:
 		super().__init__()
 		self.env_comm = ExaComm.env_comm
-		env = bsuite.load_from_id(bsuite_id='mountain_car/0')
+		# TODO: When generalizing over all envs, need to account for image and board game observation spaces.
+		# These envs (like catch and deep_sea) do not have a superfluous extra dimension.
+		# Might also want to account for bounded envs too (like catch).
+		env = bsuite.load_from_id(bsuite_id='deep_sea/0')
 		self.env = gym_wrapper.GymFromDMEnv(env)
 		self.action_space = self.env.action_space
-		obs_length = self.env.observation_space.shape[-1]
-		low = np.array([-np.inf] * obs_length)
-		high = np.array([np.inf] * obs_length)
-		self.observation_space = spaces.Box(low, high, dtype=np.float32)
+		self.has_extra_dim = False
+		if self.env.observation_space.shape[0] == 1:
+			obs_length = self.env.observation_space.shape[-1]
+			low = np.array([-np.inf] * obs_length)
+			high = np.array([np.inf] * obs_length)
+			self.observation_space = spaces.Box(low, high, dtype=self.env.observation_space.dtype)
+			self.has_extra_dim = True
+		else:
+			# print("prior: ", self.env.observation_space)
+			# obs_shape = self.env.observation_space.shape
+			# low = np.full(obs_shape, -np.inf)
+			# high = np.full(obs_shape, np.inf)
+			self.observation_space = self.env.observation_space
+		
+		
 		print("action space: ", type(self.action_space))
 		print("obs space: ", type(self.observation_space))
 		print("action space size: ", self.action_space.n)
 		print("obs space: ", self.observation_space)
+		print("obs dtype: ", self.env.observation_space.dtype)
 
 
 
 	def step(self, action) -> _GymTimestep:
 		time.sleep(0)
 		next_state, reward, done, info = self.env.step(action)
-		# print(next_state)
-		# return self.env.step(action)
-		return next_state[0], reward, done, info
+		# print("state type: ", next_state.dtype)
+		if self.has_extra_dim:
+			next_state = next_state[0]
+		return next_state, reward, done, info
 
 	def reset(self) -> np.ndarray:
 		return self.env.reset()[0]
