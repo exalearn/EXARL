6f83d0955d0ce55c278fdd113136b8ec020e32ed
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2022-01-06 15:37:43 -0700

    updating build script to work on darwin

--- setup/build_exarl.sh
+++ setup/build_exarl.sh
@@ -6,23 +6,38 @@ set -e
 target=${EXARL_TARGET:-$(hostname --fqdn)}
 echo "target = $target"
 
-if [[ $target=*"summit"* ]]
-then
+if [[ $target == *"summit"* ]]; then
     module purge
     module load cuda/11.0.3
     module load open-ce/1.4.0-py37-0
     module load git-lfs
-
-    conda env create -f setup/environment.yml
-    
-elif [[ $target="darwin"* ]]
-then
-    module load openmpi/3.1.0-gcc_7.3.0
-    module load anaconda/Anaconda3.2019.10
-    module load cuda/10.1
-
-    conda create -name exarl python=3.7
+    conda env create -f setup/summit_environment.yml
+elif [[ $target == "darwin"* ]]; then
+    module load openmpi/3.1.6-gcc_9.4.0
+    module load miniconda3/py37_4.9.2
+    module load cuda/11.4.2
+    conda create --name exarl_darwin
+    source activate exarl_darwin
     pip install -e setup/ --user
 else
     echo "Don't recognize this target/hostname: $(target)."
+    PYTHON_VERSION=3.7
+    # Use the miniconda installer for setup of conda itself
+    pushd .
+    cd
+    mkdir -p download
+    cd download
+    if [[ ! -f miniconda.sh ]]; then
+        wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+            -O miniconda.sh
+    fi
+    chmod +x miniconda.sh && ./miniconda.sh -b -f
+    conda update --yes conda
+    echo "Creating conda environment."
+    conda create -n exarl_$target --yes python=$PYTHON_VERSION
+    cd ..
+    popd
+    # Activate the python environment
+    source activate exarl_$target
+    pip install -e setup/ --user
 fi
