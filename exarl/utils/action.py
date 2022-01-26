167975cac8ffa81475ad033dbdd851d62cf64350
aik07 <ai.kagawa@gmail.com>
2022-01-26 16:21:07 -0500

    reformat the comments to match the numpy docstring format for exarl/utils/action.py

--- exarl/utils/action.py
+++ exarl/utils/action.py
@@ -1,42 +1,58 @@
-import sys
-import numpy as np
-import logging
+# -*- coding: utf-8 -*-
+"""Convert a continuous valued array to its discretized array
 
-logging.basicConfig(level=logging.INFO)
-
-'''
 This Action class convert a continuous valued array to its discretized array
 using discrete uniform distribution
 
-# How to use this class
-  # Set the class with the following three required arrays
+Example
+-------
+  * Set the class with the following three required arrays
+
     action = Action(
-        arrLower=arrLower,            # an arry containing lower value for each dimension
-        arrUpper=arrUpper,            # an arry containing upper value for each dimension
-        arrNumClasses=arrNumClasses)  # an arry containing # of disretized classes in each dimension
-  # Convert the continuous valued array (arrContAction) to its discretized array (arrDiscAction)
-  # using discrete uniform distribution
+        arrLower=arrLower,            # a 1D arry containing lower value for each dimension
+        arrUpper=arrUpper,            # a 1D arry containing upper value for each dimension
+        arrNumClasses=arrNumClasses)  # a 1D arry containing # of disretized classes in each dimension
+
+  * Convert the continuous valued array (arrContAction) to its discretized array (arrDiscAction)
+    using discrete uniform distribution
+
     arrDiscAction = action.descretize(arrContAction)
-'''
+
+"""
+
+import sys
+import numpy as np
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 class Action(object):
 
-    '''
-    A user set the following inputs:
-    * arrLower: an arry containing lowver value for each dimension
-    * arrUpper: an arry containing upper value for each dimension
-    * arrNumClasses: an arry containing # of disretized classes in each dimension
-    '''
     def __init__(self, arrLower, arrUpper, arrNumClasses):
+        """
+
+        Parameters
+        ----------
+        arrLower : a 1D array of double
+            a 1D arry containing lowver value for each dimension
+        arrUpper : a 1D array of double
+            a 1D arry containing upper value for each dimension
+        arrNumClasses : a 1D array of int
+            a 1D arry containing # of disretized classes in each dimension
+
+        Returns
+        -------
+        None
+        """
 
         self.arrLower = arrLower  # a list containing lower value for each dimension
         self.arrUpper = arrUpper  # a list containing upper value for each dimension
 
         self.arrNumClasses = arrNumClasses  # # of disretized classes in each dimension
 
-        self.dim_action = 0  # dimension of the actions (arrUpper-arrUpper)
+        self.dim_action = 0  # dimension of the actions
 
-        self.arrIntervals = []  # action interaval array
+        self.arrIntervals = []  # action interaval array (arrUpper-arrUpper)
 
         self.arrDiscAction = []  # discretized action array
 
@@ -46,8 +62,17 @@ class Action(object):
 
         self.setArrIntervals()
 
-    # set the dimension of action space
     def setDimAction(self):
+        """ This function verifies and sets the dimension of action space.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
 
         if (len(self.arrLower) != len(self.arrUpper)):
             print("ERROR: The dimension of the arrLower and arrUpper should be equal!")
@@ -58,21 +83,39 @@ class Action(object):
                   "and arrNumClasses should be equal!")
             sys.exit()
 
-        self.dim_action = len(self.arrNumClasses)  # set the dimension of the action array
+        # set the dimension of the action array
+        self.dim_action = len(self.arrNumClasses)
 
         logging.debug(f"dim_action: {self.dim_action}")
 
-    # validate the lower and upper arrays
     def checkLowerUpperArray(self):
+        """ This function validates the lower and upper arrays
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
 
         for i in range(self.dim_action):
             if (self.arrLower[i]) > (self.arrUpper[i]):
                 print("ERROR: arrLower[i] <= arrUpper[i] for all i!")
                 sys.exit()
 
-    # set interval for each action dimension, arrIntervals
     def setArrIntervals(self):
+        """ This function sets interval for each action dimension, arrIntervals
 
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
         self.arrIntervals = np.zeros(self.dim_action)
 
         for i in range(self.dim_action):
@@ -81,11 +124,19 @@ class Action(object):
 
         logging.debug(f"arrIntervals: {self.arrIntervals}", )
 
-    # discreteize continuous action using discrete uniform distribution
-    # Input:  arrContAction: 1D arry of continuous action values
-    # Output: arrDiscValues: 1D arry of discritizecd action values
     def descretize(self, arrContAction):
+        """This function discreteize continuous action using discrete uniform distribution.
 
+        Parameters
+        ----------
+        arrContAction : a 1D array of double
+            a 1D arry of continuous action values
+
+        Returns
+        -------
+        a 1D array of int
+            a 1D arry of discritizecd action values (arrDiscAction)
+        """
         if (len(arrContAction) != self.dim_action):
             print("ERROR: The dimension of actions, dim_action, "
                   "and cont_action should be equal!")
