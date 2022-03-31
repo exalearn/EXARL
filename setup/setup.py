f06a2c14c95087f24b3cdec158d7edb068a320ad
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2022-03-31 14:36:58 -0600

    fix flake8 errors in setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -4,6 +4,6 @@ setup(name='exarl',
       version='0.0.1',
       description='ExaRL is a high performance reinforcement learning framework',
       install_requires=['tensorflow-gpu>=2.0.0', 'mpi4py', 'gym==0.15.4', 'ase', 'plotille',
-                        'Lmfit', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest', 
+                        'Lmfit', 'scikit-learn', 'pandas', 'numba', 'pybind11', 'pytest',
                         'pytest-cov']
       )
