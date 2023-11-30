debce60ccd779174503285d20872c4ce5f0a6e0f
Mike Grosskopf <mike.grosskopf@gmail.com>
2023-11-29 22:04:04 -0700

    Merge in the nStep buffer to the branch compatible with new gym

--- exarl/agents/replay_buffers/buffer.py
+++ exarl/agents/replay_buffers/buffer.py
@@ -1,17 +1,20 @@
 from exarl.agents.replay_buffers.replay_buffer import ReplayBuffer, SimpleBuffer
 from exarl.agents.replay_buffers.prioritized_replay import PrioritizedReplayBuffer
 from exarl.agents.replay_buffers.trajectory_buffer import TrajectoryBuffer
+from exarl.agents.replay_buffers.nStep_buffer import nStepBuffer
 from exarl.utils.globals import ExaGlobals
 
 class Buffer:
     _builders = {"ReplayBuffer": ReplayBuffer,
                  "PrioritizedReplayBuffer": PrioritizedReplayBuffer,
                  "TrajectoryBuffer": TrajectoryBuffer,
-                  "SimpleBuffer" : SimpleBuffer}
+                 "nStepBuffer": nStepBuffer,
+                 "SimpleBuffer" : SimpleBuffer}
 
     _config_args = {"ReplayBuffer": [],
                     "PrioritizedReplayBuffer": [],
                     "TrajectoryBuffer": ['buffer_trajectory_length'],
+                    "nStepBuffer":["horizon", "gamma"],
                     "SimpleBuffer" : [] }
 
     def create(key=None, **kwargs):
